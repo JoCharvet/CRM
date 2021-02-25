4488319bdd6482e3c4230788098c5954a5f325c6
CharvetJohnny <charvetjohnny@outlook.fr>
2021-02-25 14:03:50 +0100

    tuto js de zéro

---
+++ Exo/tuto_javadezero/module-debutant/cours/06 - Les nombres/exercices.js
@@ -0,0 +1,27 @@
+/*
+ * Tapez votre code sous chacun des commentaires.
+ * Vous devez faire les exercices dans l'ordre !
+ *
+ * Si vous rencontrez un bogue, contactez-moi par email à l'adresse
+ * jeremy@javascriptdezero.com.
+ *
+ * Merci et bon courage ! 🤘
+ */
+
+// Exercice 0
+console.log(28+6)
+// Exercice 1
+console.log((6*4+2)/2)
+// Exercice 2
+console.log((1337+256)*2)
+// Exercice 3
+console.log((1337+256)*1.5+500)
+// Exercice 4
+console.log(((35-23)/2)*3)
+// Chassez le bogue 0
+
+ console.log(1000+ 2350 - (2350 * 12 / 100));
+
+// Chassez le bogue 1
+// Retirez les commentaires devant le code de Tom pour commencer
+ console.log('Montant récolté auprès des 5 investisseurs : 5 x 470 = ' + 5 * 470 + ' €\nMontant récolté auprès de 10 amis : 10 x 100 = ' + 10 * 100 + ' €\nMontant total récolté : 2350 + 1000 = ' + (2350 + 1000)+ ' €\nBénéfice net (après le prélèvement des taxes) : 2350 - 2350 * 0.12 + 1000 = ' + (2350 - 2350 * 0.12 + 1000) + ' €');
---
+++ Exo/tuto_javadezero/module-debutant/cours/09 - Le débogueur/programme.js
@@ -0,0 +1,3 @@
+let phrase ="Le débogeur c'est ";
+let adjectif ="super";
+console.log(phrase + adjectif);
