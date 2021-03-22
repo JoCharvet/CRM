da41728ddb95c21c2106fe5dd7d7efc530347ae6
Johnny Charvet <charvetjohnny@outlook.fr>
2021-03-22 16:55:24 +0100

    a

--- Exo/20-exo-final-baker/baker.js
+++ Exo/20-exo-final-baker/baker.js
@@ -9,6 +9,7 @@ com.price_baguette=0;
 com.reward=0;
 com.timer_to_accept=0;
 com.timer_to_finish=0;
+com.etat= "En attente..";
 
 var comdep=[];
 
@@ -29,15 +30,14 @@ const myBaker = {
     },
     mounted() {
 
-
+        
     },
     methods: {
         ouvrir_Boulangerie(){
-
             this.baker.open=true;
+            if(!this.baker.lost){    
 
-            if(this.baker.open===true && this.baker.lost===false){               
-                 this.inter =setInterval(() =>{
+                this.inter =setInterval(() =>{
                      this.date=new Date();           
                      this.baker.fonctionner();
                     if(this.date.getMilliseconds() %2===0){               
@@ -48,24 +48,27 @@ const myBaker = {
                 },1000);              
             }
             else{
-                this.baker=false;
+                
                 clearInterval(this.inter);
             }
         },
+
         //fonction qui permet de mettre en pause le jeu , de fermer la boulangerie
         fermer_Boulangerie(){
-            
+            clearInterval(this.inter);
             this.baker.open=false;
         },
+
         //fonction qui crée une nouvelle commande
         creation_Commande(){
-                this.commandes.splice(this.commandes.find(commande => 
-                     (commande.etat==="Refusée"|| commande.etat==="Annulée..." ||
-                      commande.etat === "Livrée")||(commande.etat==="En attente..." 
+                this.commandes.splice(this.commandes.findIndex(commande => 
+                     (commande.etat==="Refusée"|| commande.etat==="Annulée" ||
+                      commande.etat === "Livrée")||(commande.etat==="En attente.." 
                       && commande.id===0)),1,new Commandes(this.baker.level , this.id_Count));
                 this.id_Count++;
             
         },
+
         //fonction qui parcours le tableau de commandes et gère les opération a faire sur les commandes en fonction des timers et du pain disponible
         gestion_Commande(){
             this.commandes.forEach(commande => {
@@ -90,22 +93,26 @@ const myBaker = {
                
             });
         },
+
         //fonction qui change l'état de la commande en annulée
         refuser_Commande(commande){
             commande.etat="Refusée";
         },
+
         //fonction qui change l'état de la commande en acceptée
         accepter_Commande(commande){
-            commande.etat="Acceptée";
+            commandes.find(com => com.id =commande.id ).etat="Acceptée";
             this.ajouter_Log("La commande "+commande.id+" a été acceptée");
         },
+
         //fonction qui annule la commande si un des timer est arrivé a 0; 
         annuler_Commande(commande){
-            if(commande.timer_to_finish===0||commande.timer_to_accept===0)
+            if(commande.timer_to_finish<=0||commande.timer_to_accept<=0)
             {
                 commande.etat="Annulée";
             }
         },
+
         //fonction qui livre la commande si le stock de pain de la boulangerie le permet
         livrer_Commande(commande){
             if(this.baker.baguettes >=  commande.nb_baguettes)
@@ -117,8 +124,10 @@ const myBaker = {
                 this.ajouter_Log("La boulangerie a livré "+commande.nb_baguettes+" baguettes");
             }
         },
+
+        // fonction qui insere les logs en rajoutant l'heure
         ajouter_Log(str){
-            this.log.unshift(date.toLocaleTimeString()+": "+str)
+        this.log.unshift(date.toLocaleTimeString()+": "+str)
         }
     },
 };
